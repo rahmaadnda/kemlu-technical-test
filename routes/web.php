1eddffd39c20e4799d96d02babab83e61bb51f04
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-08-17 23:15:09 +0700

    redeploy

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/geomap', function () {
-    return view('geomap'); // Adjust to your actual view file
+    return view('geomap'); 
 })->name('geomap');
 
 Route::get('/datatable', function () {
-    return view('datatable'); // Adjust to your actual view file
+    return view('datatable');
 })->name('datatable');
