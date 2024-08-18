10b8ec5682386acec5d1ac1485386feee0a41c5b
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-08-18 17:32:54 +0700

    add title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center vh-100">
     <div class="text-center">
+        <h1 class="mb-4">Technical Test MSIB 7 Kementerian Luar Negeri RI</h1>
+        <h2 class="mb-4">Rahma Adinda Putri</h2>
         <a href="{{ route('geomap') }}" class="btn btn-warning btn-lg" style="margin-right: 1rem;">Lihat Geomap</a>
         <a href="{{ route('datatable') }}" class="btn btn-warning btn-lg">Lihat Daftar Negara</a>
     </div>
